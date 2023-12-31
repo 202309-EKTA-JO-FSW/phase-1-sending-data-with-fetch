efd1221a3bbd2f22d31a19f4cae742ce09bf54cb
Ahmed Ibrahim Ahmed <ahmed7cf@gmail.com>
2023-12-31 20:38:14 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Add your code here
+
+
+function submitData(username, password) {
+    fetch("http://localhost:3000/users", {
+        method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+    },
+    body: JSON.stringify({
+        name: username,
+        email: password,
+    }),
+})
+    .then((response) => response.json())
+    .then((jsonReturned) => console.log(jsonReturned.id))
+    .catch(function (error) {
+        alert("Bad things!");
+        console.log(error.message);
+      })};
